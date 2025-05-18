28b3d55d1c1ae47a4f4237d5c166fa53c78e0721
Vishnu <vishnuvr3377@gmail.com>
2025-05-18 20:39:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, url_for, request, redirect
 import csv
 
 app = Flask(__name__)
-print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
